e996a650befc475d6cc4d93c54236d461d21909d
Santiago Antón <santiagoanton@Santiagos-MacBook-Air.local>
2023-10-03 15:02:01 -0600

    Finished the Unicafe exercises.

--- Part1/exercises1.6-1.14/src/App.jsx
+++ Part1/exercises1.6-1.14/src/App.jsx
@@ -2,10 +2,89 @@ import { useState } from 'react'
 
 const Button = props => <button onClick={props.handleClick}>{props.buttonName} {props.stateValue}</button>
 
-const AverageComponent = props => {
+const StatisticLine = props => {
+  
+  const goodState = props.feedbacks.goodState
+  const neutralState = props.feedbacks.neutralState
+  const badState = props.feedbacks.badState
+  const totalCount = goodState + neutralState + badState
+
+  if (props.text === "Good feedback")
+    return (
+      <tr>
+        <td>{props.text}</td>
+        <td>{goodState}</td>
+      </tr>
+    )
+  if (props.text === "Neutral feedback")
   return (
-    <div>Average score: {(props.goodCount - props.badCount) / props.totalCount}</div>
+    <tr>
+      <td>{props.text}</td>
+      <td>{neutralState}</td>
+    </tr>
   )
+  if (props.text === "Bad feedback")
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{badState}</td>
+    </tr>
+  )
+  if (props.text === "Total feedbacks received")
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{goodState + neutralState + badState}</td>
+    </tr>
+  )  
+  if (props.text === "Average score")
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{(goodState - badState) / totalCount}</td>
+    </tr>
+  )
+  if (props.text === "Positive percentage")
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{(goodState*100)/totalCount}%</td>
+    </tr>
+  )  
+  else
+    return (
+      <td>Could not match the component text to any component</td>
+    )
+}
+
+const Statistics = props => {
+  
+  let totalCount = props.goodState + props.neutralState + props.badState
+
+  if (totalCount === 0) 
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>No feedback has been submitted</p>
+      </div>
+    )
+
+  else 
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <table>
+          <tbody>
+            <StatisticLine text="Good feedback" feedbacks={props}/>
+            <StatisticLine text="Neutral feedback" feedbacks={props}/>
+            <StatisticLine text="Bad feedback" feedbacks={props}/>
+            <StatisticLine text="Total feedbacks received" feedbacks={props}/>
+            <StatisticLine text="Average score" feedbacks={props}/>  
+            <StatisticLine text="Positive percentage" feedbacks={props}/>
+          </tbody>
+        </table>
+      </div>
+    )
 }
 
 const App = () => {
@@ -26,15 +105,28 @@ const App = () => {
         <Button handleClick={countNeutral} buttonName={"Give neutral feedback"}/>
         <Button handleClick={countBad} buttonName={"Give bad feedback"}/>
       </p>
-      <h2>Statistics</h2>
-      <p>Good feedback: {good}</p>
-      <p>Neutral feedback: {neutral}</p>
-      <p>Bad feedback: {bad}</p>
-      <p>Total feedbacks received: {good + neutral + bad}</p>
-      <AverageComponent goodCount={good} badCount={bad} totalCount={good + neutral + bad}/>  
-      <p>Positive percentage: {(good*100)/(good+neutral+bad)}%</p>
+      <Statistics goodState={good} neutralState={neutral} badState={bad}/>
     </div>
   )
 }
 
 export default App
+
+/** table structure 
+ * 
+ * <table>
+ * <tbody>
+ *  <tr>        table row
+ *    <th>      table headers
+ *    <th>
+ *    <th>
+ *  </tr>
+ *  <tr>        first row of data
+ *    <td>      table data
+ *    <td>
+ *    <td>
+ *  </tr>
+ * </tbody>
+ * </table>
+ * 
+ */
